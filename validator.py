015b2f9c429e3245dbeeb04012cd338b3fbc08c8
Adrian52507 <adrian52507@gmail.com>
2025-06-05 13:21:16 -0500

    Agregar Validador y Pruebas2

--- validator.py
+++ validator.py
@@ -4,5 +4,5 @@ def is_valid_address(address):
     if not isinstance(address, str):
         return False
     # Simple check: must contain a street name and number
-    pattern = r'^[A-Za-z\s]+\s\d+$' # r'^[A-Za-zÁÉÍÓÚáéíóúÑñ\s\.\-]+ \d+$' -> Permite letras, tildes, espacios, puntos, guiones, y un número al final
+    pattern = r'^[A-Za-zÁÉÍÓÚáéíóúÑñ\s\.\-]+ \d+$' # r'^[A-Za-zÁÉÍÓÚáéíóúÑñ\s\.\-]+ \d+$' -> Permite letras, tildes, espacios, puntos, guiones, y un número al final
     return bool(re.match(pattern, address.strip()))
